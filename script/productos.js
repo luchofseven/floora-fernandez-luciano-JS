36fc6d51625f0729078ae5b13d00540f8fe24525
Luciano Fernandez <luchofseven@gmail.com>
2022-05-06 15:16:12 -0300

    mostrando cantidad

--- script/productos.js
+++ script/productos.js
@@ -6,105 +6,118 @@ const products = [
     img: "../img/labial-gloss.jpg",
     name: "Brillo Labial",
     description: "Brillo labial ultra brillante de acabado húmedo. Hidrata sin ser pegajoso. Crea un efecto de volumen.",
-    price: 395
+    price: 395,
+    cant: 0
   },
   {
     id: 2,
     img: "../img/labial-liquido.jpg",
     name: "Labial Líquido",
     description: "Labial líquido de color intenso y acabado satinado. Híbrido que cumple la función de labial y gloss a la vez.",
-    price: 385
+    price: 385,
+    cant: 0
   },
   {
     id: 3,
     img: "../img/labial-holografico.jpg",
     name: "Labial Holográfico",
     description: "Brillo labial holográfico ultra brillante con pigmentos móviles que se transfroman en color.",
-    price: 390
+    price: 390,
+    cant: 0
   },
   {
     id: 4,
     img: "../img/labial-aterciopelado.jpg",
     name: "Labial Aterciopelado",
     description: "Labial líquido de fórmula ligera y alta pigmentación. Hidratante, cremoso y de acabado aterciopelado.",
-    price: 385
+    price: 385,
+    cant: 0
   },
   {
     id: 5,
     img: "../img/ojos-fibra.jpg",
     name: "Delineador Fibra",
     description: "Delineador en fibra. Negro Intenso. Larga duración y resistente al agua.",
-    price: 295
+    price: 295,
+    cant: 0
   },
   {
     id: 6,
     img: "../img/ojos-mascara.jpg",
     name: "Máscara de Pestañas",
     description: "Máscara de pestañas waterproof. Negro intenso. Crea volumen y alarga las pestañas.",
-    price: 395
+    price: 395,
+    cant: 0
   },
   {
     id: 7,
     img: "../img/ojos-lapiz.jpg",
     name: "Lápiz Delineador",
     description: "Delineador en lápiz negro intenso. Textura supercremosa que se aplica sin esfuerzo. Larga duración.",
-    price: 150
+    price: 150,
+    cant: 0
   },
   {
     id: 8,
     img: "../img/ojos-sombras.jpg",
     name: "Paleta Pastel",
     description: "Paleta con gama de todos mates, superiridiscentes y metalizados que permiten lograr diferentes looks.",
-    price: 685
+    price: 685,
+    cant: 0
   },
   {
     id: 9,
     img: "../img/rostro-polvo.jpg",
     name: "Polvo Volátil",
     description: "Polvo suelto ultraligero que fija y prolonga la duración del maquillaje.",
-    price: 450
+    price: 450,
+    cant: 0
   },
   {
     id: 10,
     img: "../img/rostro-base.jpg",
     name: "Base Líquida",
     description: "Base fluída de alta cobertura. No reseca la piel. Larga duración.",
-    price: 480
+    price: 480,
+    cant: 0
   },
   {
     id: 11,
     img: "../img/rostro-bronzer.jpg",
     name: "Paleta de Bronzers",
     description: "Paleta de bronceadores mates y brillantes para dar profunidad y dimensión.",
-    price: 500
+    price: 500,
+    cant: 0
   },
   {
     id: 12,
     img: "../img/rostro-iluminador.jpg",
     name: "Paleta de Iluminadores",
     description: "Paleta de cuatro iluminadores, para conseguir desde un brillo natural hasta un mega glow.",
-    price: 500
+    price: 500,
+    cant: 0
   },
   {
     id: 13,
     img: "../img/esmalte-colores.jpg",
     name: "Esmaltes",
     description: "Esmalte para uñas. 24 Tonos.",
-    price: 200
+    price: 200,
+    cant: 0
   },
   {
     id: 14,
     img: "../img/esmaltes-matificante.jpg",
     name: "Esmalte Matificante",
     description: "Protege el esmalte y proporciona a la uña un acabado efecto mate sobre cualquier esmalte común.",
-    price: 300
+    price: 300,
+    cant: 0
   }
 ]
 
 productsEnCarrito = localStorage.getItem("carrito")
 
-productsEnCarrito === null ? productsEnCarrito = [] : productsEnCarrito = JSON.parse(productsEnCarrito)
-
+productsEnCarrito = productsEnCarrito === null ? [] : JSON.parse(productsEnCarrito)
 
 const divProducts = document.querySelector("#div-products")
 const divProductsRow = document.querySelector("#div-products-row")
@@ -198,7 +211,12 @@ function mostrarProductos() {
 
 function agregarProducto(id) {
   const productosAgregados = products.find(products => products.id === id)
-  productsEnCarrito.push(productosAgregados)
+  if (productsEnCarrito.find(products => products.id === id) === productosAgregados) {
+    productosAgregados.cant++
+  } else {
+    productosAgregados.cant = 1
+    productsEnCarrito.push(productosAgregados)
+  }
   let carritoJson = JSON.stringify(productsEnCarrito)
   localStorage.setItem("carrito", carritoJson)
   mostrarCarrito(productsEnCarrito)
@@ -240,7 +258,7 @@ function mostrarCarrito(carrito) {
     cardText.textContent = product.description
 
     const cardPrice = document.createElement('h3')
-    cardPrice.textContent = "$" + product.price
+    cardPrice.textContent = "$" + (product.price * product.cant)
 
     const button = document.createElement('button')
     button.type = 'button'
@@ -257,11 +275,16 @@ function mostrarCarrito(carrito) {
         }
       }).showToast();
     }
-  
+    
+    const cantidad = document.createElement('h5')
+    cantidad.className = 'h5-cantidad'
+    cantidad.textContent = product.cant
+
     divCardBody.appendChild(cardTitle)
     divCardBody.appendChild(cardText)
     divCardBody.appendChild(cardPrice)
     divCardBody.appendChild(button)
+    divCardBody.appendChild(cantidad)
     
     divColMd.appendChild(divCardBody)
 
@@ -279,8 +302,13 @@ function mostrarCarrito(carrito) {
 }
 
 function eliminarProducto(id) {
-  const productoEliminado = productsEnCarrito.findIndex(product => product.id === id)
-  productsEnCarrito.splice(productoEliminado, 1)
+  const productoEliminado = productsEnCarrito.findIndex(products => products.id === id)
+  const productoAEliminar = productsEnCarrito.find(products => products.id === id)
+  if (productoAEliminar.cant === 1) {
+    productsEnCarrito.splice(productoEliminado, 1)
+  } else {
+    productoAEliminar.cant--
+  }
   let carritoJson = JSON.stringify(productsEnCarrito)
   localStorage.setItem("carrito", carritoJson)
   mostrarCarrito(productsEnCarrito)
